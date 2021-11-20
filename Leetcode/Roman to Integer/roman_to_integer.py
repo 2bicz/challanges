2c2f3fb6d0ce49a39e96517c20ba5e2dee3f056a
Patryk Tubicz <patryk.tubicz@wp.pl>
2021-11-20 20:59:59 +0100

    Add Roman to Integer Python & Java solutions with description

---
+++ Leetcode/Roman to Integer/roman_to_integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {
+            'M': 1000,
+            'D': 500,
+            'C': 100,
+            'L': 50,
+            'X': 10,
+            'V': 5,
+            'I': 1
+        }
+        result = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            result += values[char]
+        return result
